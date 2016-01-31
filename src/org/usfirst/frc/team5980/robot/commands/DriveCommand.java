d231ee4b3f673412a96b135345f59c76a617c866
Ludwant <antonludwig01@yahoo.com>
2016-01-31 18:00:10 -0500

    Created Repository

---
+++ src/org/usfirst/frc/team5980/robot/commands/DriveCommand.java
@@ -0,0 +1,41 @@
+package org.usfirst.frc.team5980.robot.commands;
+
+import org.usfirst.frc.team5980.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+
+/**
+ *
+ */
+public class DriveCommand extends Command {
+
+    public DriveCommand() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    	requires(Robot.drive);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	Robot.drive.setDrivePower(Robot.oi.joystickXbox.getRawAxis(1), Robot.oi.joystickXbox.getRawAxis(5));
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ src/org/usfirst/frc/team5980/robot/subsystems/DriveSubsystem.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team5980.robot.subsystems;
+
+import org.usfirst.frc.team5980.robot.commands.DriveCommand;
+
+import edu.wpi.first.wpilibj.Victor;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+/**
+ *
+ */
+public class DriveSubsystem extends Subsystem {
+    Victor leftBackMotor = new Victor(0);
+    Victor leftFrontMotor = new Victor(1);
+    Victor rightBackMotor = new Victor(2);
+    Victor rightFrontMotor = new Victor(3);
+    
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+    public void setDrivePower(double leftPower, double rightPower) {
+    	leftBackMotor.setInverted(true);
+    	leftFrontMotor.setInverted(true);
+    	leftBackMotor.set(leftPower);
+    	leftFrontMotor.set(leftPower);
+    	rightBackMotor.set(rightPower);
+    	rightFrontMotor.set(rightPower);
+    }
+    
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+        setDefaultCommand(new DriveCommand());
+    }
+}
+
